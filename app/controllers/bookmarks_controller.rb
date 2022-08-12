b82b2abcac9e2a357d25880aef9d67feb05aaf7c
Nic Rothquel <nrothquel@gmail.com>
2022-08-12 12:17:42 -0400

    added delete bookmark functionality

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -10,6 +10,9 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
   end
 
   def index
